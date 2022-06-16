9774b1d778d9493c6ff6b8eccddfdbffa9e99056
reddyarun59 <reddyarun59@yahoo.com>
2022-06-16 22:20:27 +0530

    various types of variables

--- Variables/main.go
+++ Variables/main.go
@@ -8,4 +8,32 @@ func main() {
 	var i int = 42
 
 	fmt.Println(i)
+
+	fmt.Printf("Variable is of type %T \n", i)
+
+	var username string = "Arun"
+	fmt.Println(username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type %T \n", isLoggedIn)
+
+	// default values and alies
+
+	var anotherVar string
+	fmt.Println(anotherVar)
+	fmt.Printf("Variable is of type %T \n", anotherVar)
+
+	// implicit type
+
+	var something = "Hello World"
+	fmt.Println(something)
+	fmt.Printf("Variable is of type %T \n", something)
+	// something = 3
+	// fmt.Println(something)
+
+	// no var style
+
+	newVar := 32
+	fmt.Println(newVar)
 }
